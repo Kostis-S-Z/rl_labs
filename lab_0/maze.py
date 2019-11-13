d8fce697dcafb48f01b68134c6179cda83429eff
AlexLacson <alexander_nou@hotmail.com>
2019-11-13 11:50:29 +0100

    Works with T now

--- lab_0/maze.py
+++ lab_0/maze.py
@@ -34,7 +34,7 @@ for i in range(maze.shape[0]):
         if (i - 1, j) in states:
             value.append((i - 1, j))
         actions[key] = value
-        u[key] = -420
+        u[key] = (-420, None, None)
 
 print("Actions:", actions)
 
@@ -42,17 +42,24 @@ print("Actions:", actions)
 rewards = {}
 for state in states:
     if state == (5, 5):
-        rewards[state] = 0
+        rewards[state] = 0.0
+    elif state == (5, 0):
+        rewards[state] = -7*0.5 - 1*0.5
+    elif state == (3, 6):
+        rewards[state] = -2*0.5 -1 *0.5
     else:
-        rewards[state] = -1
+        rewards[state] = -1.0
 
-start_state = (5, 5)
-state_list = [start_state]
-T = 50
+#start_state = (5, 5)
+#state_list = [start_state]
+T = 20
 
-u[(5, 5)] = (0, (5,5))
+u[(5, 5)] = (0, (5,5), T)
+state_list = []
+for possible_state in actions[(5, 5)]:
+    state_list.append(possible_state)
 
-while len(state_list) != 0 and T != 0:
+while len(state_list) != 0:
 
     cur_state = state_list.pop(0)
     print("Current state:", cur_state)
@@ -62,19 +69,19 @@ while len(state_list) != 0 and T != 0:
     best_action = None
 
     for possible_state in possible_states:
-        if u[possible_state] != -420:
+        if u[possible_state][0] != -420:
             reward = rewards[possible_state] + u[possible_state][0]
             if reward > max_reward:
                 max_reward = reward
                 best_action = possible_state
+                t = u[possible_state][2]
 
-    if max_reward != -420:
-        u[cur_state] = (max_reward, best_action)
-        for possible_state in possible_states:
-            if u[possible_state] == -420:
+    if max_reward > u[cur_state][0]:
+        u[cur_state] = (max_reward, best_action, t-1)
+        if t-1 > 0:
+            for possible_state in possible_states:
                 state_list.append(possible_state)
 
-#     T -= 1
 print("U:", u)
 
 cur_state = (0, 0)
@@ -83,3 +90,6 @@ while cur_state != (5, 5):
     cur_state = u[cur_state][1]
     path.append(cur_state)
 print('Path: ', path)
+
+maze[np.array(path)[:, 0], np.array(path)[:, 1]] = 9
+print(maze)
