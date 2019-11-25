18d1001c866e61ee56694926c0f6b046185b4aef
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-11-25 15:48:14 +0100

    Q learning works

--- lab_1/bank_robbing.py
+++ lab_1/bank_robbing.py
@@ -9,10 +9,10 @@ Heist a bank while escaping from the police!
 
 min_val = np.iinfo(np.int16).min
 max_val = np.iinfo(np.int16).max
-n_actions = len(["stay", "up", "down", "right", "left"])
+n_actions = 5  # stay, up, down, left, right
 
-robber = (0, 0)
-police = (3, 3)
+init_robber = (0, 0)
+init_police = (3, 3)
 bank = (1, 1)
 
 reward_map = np.array([
@@ -24,9 +24,9 @@ reward_map = np.array([
 reward_map[bank] = 1
 
 # Parameter values
-iterations = 10000
+iterations = 10000000
 epsilon = 0.1
-discount_factor = 0.8
+d_factor = 0.8
 
 
 def build_state_space():
@@ -49,19 +49,28 @@ def build_action_space(states):
     for i in range(reward_map.shape[0]):
         for j in range(reward_map.shape[1]):
             key = (i, j)
-            value = [(i, j)]
+            value = [((i, j), "stay")]
 
             if (i, j - 1) in states:
-                value.append((i, j - 1))
+                value.append(((i, j - 1), "left"))
             if (i, j + 1) in states:
-                value.append((i, j + 1))
+                value.append(((i, j + 1), "right"))
             if (i + 1, j) in states:
-                value.append((i + 1, j))
+                value.append(((i + 1, j), "down"))
             if (i - 1, j) in states:
-                value.append((i - 1, j))
+                value.append(((i - 1, j), "up"))
             actions[key] = value
 
     return actions
+
+
+def init_q(states, actions):
+    q = {}
+    for robber_state in states:
+        for police_state in states:
+            possible_actions = actions[robber_state]
+            q[robber_state, police_state] = {action[1]: 0 for action in possible_actions}
+    return q
 
 
 def reward_function(r_state, p_state):
@@ -73,18 +82,53 @@ def reward_function(r_state, p_state):
         return 0
 
 
-def q_learning():
-    action_count = np.zeros(shape=n_actions)
-    q_values = np.zeros(shape=n_actions)
+def choose_random_action(action_space, state):
+    action = np.random.choice(len(action_space[state]))
+    return action_space[state][action]
+
+
+def q_learning(states, action_space):
+
+    robber_state = init_robber
+    police_state = init_police
+    s_t = (robber_state, police_state)
+
+    q = init_q(states, action_space)
+    lr = init_q(states, action_space)
 
     for i in range(iterations):
 
-        action = e_greedy(q_values)
-        action_count[action] += 1
+        # Choose random action out of legal actions
+        next_robber_state, robber_action = choose_random_action(action_space, robber_state)
+        next_police_state, police_action = choose_random_action(action_space, police_state)
+
+        s_t_1 = (next_robber_state, next_police_state)
+
+        # Calculate reward
+        r = reward_function(next_robber_state, next_police_state)
+
+        # Find appropriate learning rate (alpha) based on current state and action
+        lr[s_t][robber_action] += 1
+        alpha = 1 / (lr[s_t][robber_action] ** (2/3))
+
+        # Update Q values
+        q[s_t][robber_action] += alpha * (r + d_factor * np.max(list(q[s_t_1].values())) - q[s_t][robber_action])
+
+        # Move to next state
+        robber_state = next_robber_state
+        police_state = next_police_state
+        s_t = (robber_state, police_state)
+
+        if i % 1000 == 0:
+            print(f"Iteration: {i} Q{s_t}: {q[s_t]}")
 
 
-def sarsa():
-    pass
+def sarsa(states):
+    action_count = np.zeros(shape=n_actions)
+    q = init_q(states)
+
+    action = e_greedy(q)
+    action_count[action] += 1
 
 
 def e_greedy(q):
@@ -97,3 +141,5 @@ def e_greedy(q):
 s_space = build_state_space()
 a_space = build_action_space(s_space)
 
+# We assume we choose only from LEGAL actions
+q_learning(s_space, a_space)
