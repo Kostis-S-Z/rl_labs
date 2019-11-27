a6e03ac95b6a0a3b37f8c3a0eef57841015c01ff
Kostis-S-Z <kostis.s.z.24@gmail.com>
2019-11-27 19:40:40 +0100

    Fix plot

--- lab_1/bank_robbing.py
+++ lab_1/bank_robbing.py
@@ -26,7 +26,7 @@ reward_map = np.array([
 reward_map[bank] = 1
 
 # Parameter values
-iterations = 10000000  # 10.000.000 = 10000000
+iterations = 5000000  # 10.000.000 = 10000000
 d_factor = 0.8
 
 
@@ -218,7 +218,7 @@ def sarsa(states, action_space_robber, action_space_police, epsilon=0.1, verbose
         police_state = next_police_state
         s_t = (robber_state, police_state)
 
-        q_max_evolution.append(np.max(q[init_state]))
+        q_max_evolution.append(np.max(list(q[init_state].values())))
         q_init_evolution["stay"].append(q[init_state]["stay"])
         q_init_evolution["down"].append(q[init_state]["down"])
         q_init_evolution["right"].append(q[init_state]["right"])
@@ -229,8 +229,8 @@ def sarsa(states, action_space_robber, action_space_police, epsilon=0.1, verbose
             print(f"Iteration: {i} ({elapsed}sec) | Q{s_t}: {q[s_t]}")
             start = time.time()
 
-    plot_q(q_init_evolution, title="q_evolution_" + str(epsilon))
-    plot_v(q_max_evolution, title="V_" + str(epsilon))
+    # plot_q(q_init_evolution, title="q_evolution_" + str(epsilon))
+    # plot_v(q_max_evolution, title="V_" + str(epsilon))
     return q_max_evolution
 
 
@@ -239,10 +239,9 @@ a_space = build_action_space(s_space)
 p_a_space = build_action_space(s_space, can_stay=False)
 
 # We assume we choose only from LEGAL actions
-q_learning(s_space, a_space, p_a_space, verbose=True)
+# q_learning(s_space, a_space, p_a_space, verbose=True)
 
-# 0.01, 0.05, 0.1
-e_values = [0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9]
+e_values = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 0.9]
 for e in e_values:
     v_evolution = sarsa(s_space, a_space, p_a_space, verbose=True, epsilon=e)
     plt.plot(range(len(v_evolution)), v_evolution, label='epsilon=' + str(e))
